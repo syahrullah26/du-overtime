49924ff348bdf29c920edee4eb8ac01e16d467eb
Faris Andika Putra <farisandika2111@gmail.com>
2026-02-10 16:36:23 +0700

    minor fix: routing

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Department;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
