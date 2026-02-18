a798bb09a8b65adf3481201e99daa357ab804406
Muhammad Syahrulah <muhammadsyahrullah26@gmail.com>
2026-02-18 10:46:44 +0700

    submit overtime

--- frontend/app/components/base/baseTextArea.vue
+++ frontend/app/components/base/baseTextArea.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
-defineProps<{
+const model = defineModel<string>();
+
+interface Props {
   label?: string;
   placeholder?: string;
-  rows?: number;
-}>();
+  rows?: number | string;
+}
+
+withDefaults(defineProps<Props>(), {
+  rows: 4,
+  placeholder: "Tuliskan Alasan...",
+});
 </script>
+
 <template>
-  <textarea
-    class="w-full block border p-4 rounded-xl text-black transition-all hover:bg-gray-50 focus:bg-white focus:border-[var(--gold-dark)] focus:ring-2 focus:ring-[var(--gold-main)] outline-none box-border"
-    :placeholder="placeholder"
-    :rows="rows"
-  ></textarea>
+  <div class="w-full flex flex-col gap-2">
+    <label
+      v-if="label"
+      class="text-sm font-semibold text-[var(--gold-dark)] capitalize"
+    >
+      {{ label }}
+    </label>
+
+    <textarea
+      v-model="model"
+      class="w-full block border p-4 rounded-xl text-black transition-all hover:bg-gray-50 focus:bg-white focus:border-[var(--gold-dark)] focus:ring-2 focus:ring-[var(--gold-main)] outline-none box-border resize-y"
+      :placeholder="placeholder"
+      :rows="rows"
+    ></textarea>
+  </div>
 </template>
--- frontend/app/pages/overtime/index.vue
+++ frontend/app/pages/overtime/index.vue
@@ -6,6 +6,11 @@ interface SelectOption {
 
 const { userState } = useAuth();
 const { fetchUsersByRole } = useUser();
+const { submitOvertime } = useOvertime();
+
+// form utils
+const isLoading = ref(false);
+const errorMessage = ref("");
 
 const picOptions = ref<SelectOption[]>([]);
 const clevelOptions = ref<SelectOption[]>([]);
@@ -45,8 +50,40 @@ const { pending } = useAsyncData(
   },
 );
 
-const handleSubmit = () => {
+const handleSubmit = async () => {
   console.log("Data yang akan dikirim:", form.value);
+  errorMessage.value = "";
+  //validasi form
+  if (
+    !form.value.date ||
+    !form.value.startTime ||
+    !form.value.endTime ||
+    !form.value.reason ||
+    !form.value.clevel ||
+    !form.value.pic
+  ) {
+    errorMessage.value = "Please fill in all fields";
+    return;
+  }
+  isLoading.value = true;
+  try {
+    //buat payload
+    const overtimePayload = {
+      name: form.value.name,
+      position: form.value.position,
+      date: form.value.date,
+      start_time: form.value.startTime,
+      end_time: form.value.endTime,
+      reason: form.value.reason,
+      pic_id: form.value.pic,
+      clevel_id: form.value.clevel,
+    };
+    const result = await submitOvertime(overtimePayload);
+  } catch (error) {
+    errorMessage.value = "error";
+  } finally {
+    isLoading.value = false;
+  }
 };
 </script>
 <template>
@@ -70,68 +107,92 @@ const handleSubmit = () => {
       >
         <h3 class="font-bold text-xl text-gray-800">Form Pengajuan</h3>
       </div>
-
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mx-4 my-4">
-        <BaseInput
-          label="nama"
-          type="text"
-          placeholder="Nama"
-          v-model="name"
-          readonly
-          disabled
-        />
-        <BaseInput
-          label="jabatan"
-          type="text"
-          placeholder="Jabatan"
-          v-model="role"
-          readonly
-          disabled
-        />
-      </div>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-2 mx-4 mb-4">
-        <div class="group">
-          <BaseLabel label="Tanggal" />
+      <form @submit.prevent="handleSubmit">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mx-4 my-4">
           <BaseInput
-            label="tanggal"
-            type="date"
-            placeholder="Tanggal Overtime"
+            label="nama"
+            type="text"
+            placeholder="Nama"
+            v-model="name"
+            readonly
+            disabled
+          />
+          <BaseInput
+            label="jabatan"
+            type="text"
+            placeholder="Jabatan"
+            v-model="role"
+            readonly
+            disabled
           />
         </div>
-        <div class="group">
-          <BaseLabel label="Jam Mulai" />
-          <BaseInput label="jam mulai" type="time" placeholder="Jam Mulai" />
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-2 mx-4 mb-4">
+          <div class="group">
+            <BaseLabel label="Tanggal" />
+            <BaseInput
+              label="tanggal"
+              type="date"
+              placeholder="Tanggal Overtime"
+              v-model="form.date"
+              required
+            />
+          </div>
+          <div class="group">
+            <BaseLabel label="Jam Mulai" />
+            <BaseInput
+              label="jam mulai"
+              type="time"
+              placeholder="Jam Mulai"
+              v-model="form.startTime"
+              required
+            />
+          </div>
+          <div class="group">
+            <BaseLabel label="Jam Selesai" />
+            <BaseInput
+              label="jam selesai"
+              type="time"
+              placeholder="Jam Selesai"
+              v-model="form.endTime"
+              required
+            />
+          </div>
         </div>
-        <div class="group">
-          <BaseLabel label="Jam Selesai" />
-          <BaseInput
-            label="jam selesai"
-            type="time"
-            placeholder="Jam Selesai"
+        <div class="grid grid-cols-1 gap-4 mx-4 my-4">
+          <BaseTextArea
+            label="alasan"
+            placeholder="Alasan"
+            class="mb-4 mr-4"
+            v-model="form.reason"
+            :disabled="isLoading"
           />
         </div>
-      </div>
-      <div class="grid grid-cols-1 gap-4 mx-4 my-4">
-        <BaseTextArea label="alasan" placeholder="Alasan" class="mb-4 mr-4" />
-      </div>
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mx-4 my-4">
-        <BaseSelect
-          v-model="form.pic"
-          label="Pilih PIC"
-          placeholder="Pilih PIC Overtime"
-          :options="picOptions"
-        />
-        <BaseSelect
-          v-model="form.clevel"
-          label="Pilih C-Level"
-          placeholder="Pilih C-Level Overtime"
-          :options="clevelOptions"
-        />
-      </div>
-      <div class="flex justify-center items-center gap-2 my-4">
-        <BaseButton label="Submit" variant="primary" type="submit" />
-        <BaseButton label="Cancel" variant="secondary" type="button" />
-      </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mx-4 my-4">
+          <BaseSelect
+            v-model="form.pic"
+            label="Pilih PIC"
+            placeholder="Pilih PIC Overtime"
+            :options="picOptions"
+            required
+          />
+          <BaseSelect
+            v-model="form.clevel"
+            label="Pilih C-Level"
+            placeholder="Pilih C-Level Overtime"
+            :options="clevelOptions"
+            required
+          />
+        </div>
+        <div class="flex justify-center items-center gap-2 my-4">
+          <BaseButton
+            :label="isLoading ? 'Loading...' : 'Submit'"
+            variant="primary"
+            :disabled="isLoading"
+            type="submit"
+          />
+          <BaseButton label="Cancel" variant="secondary" type="button" />
+        </div>
+      </form>
     </div>
   </div>
 </template>
